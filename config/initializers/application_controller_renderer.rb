80fee9b93cb8476032ef62fa4fee2caa69c78988
lil-shimon <shimon101018@gmail.com>
2020-08-17 08:04:57 +0900

    Created recipegra,

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = '080aa840ebc853900e6ae968497c80202ae1a25f1daa66123eab8a2a7d918f81d72d14ba9c9479d5f0db1afcd87487c46dfc9c76aa88c6289145d606e33cb600'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#index"
+  resources :users
+  resources :recipes
+  #この一行で必要なルーティングが作られる
+end
